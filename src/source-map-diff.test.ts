fe603beb755c75d02b66aee29da7471d052079f6
Dylan Greene <dgreene@medallia.com>
2021-02-07 19:11:04 -0500

    add github action

--- src/source-map-diff.test.ts
+++ src/source-map-diff.test.ts
@@ -1,5 +1,6 @@
-import { sourceMapDiff } from './source-map-diff';
+import { sourceMapDiff, sourceMapDiffAsHtml, sourceMapDiffForConsole } from './source-map-diff';
 
+jest.mock('./generate-tree', () => ({ generateTree: (diff: unknown) => diff }));
 jest.mock('./load-source-map', () => ({
     loadSourceMap: (name: string) =>
         name === 'current.js'
@@ -9,7 +10,67 @@ jest.mock('./load-source-map', () => ({
 
 describe('source-map-diff', () => {
     test('sourceMapDiff', async () => {
-        const { diff } = await sourceMapDiff({ currentSrc: 'current.js', previousSrc: 'previous.js' });
+        const diff = await sourceMapDiff({ currentSrc: 'current.js', previousSrc: 'previous.js' });
+        expect(diff).toEqual([
+            {
+                added: false,
+                changeInSize: 9900,
+                currentSize: 10000,
+                isDecreased: false,
+                isIncreased: true,
+                isSame: false,
+                name: 'src/bigger',
+                path: ['src', 'bigger'],
+                previousSize: 100,
+                removed: false
+            },
+            {
+                added: false,
+                changeInSize: -1800,
+                currentSize: 200,
+                isDecreased: true,
+                isIncreased: false,
+                isSame: false,
+                name: 'src/smaller',
+                path: ['src', 'smaller'],
+                previousSize: 2000,
+                removed: false
+            }
+        ]);
+    });
+
+    test('sourceMapDiffAsHtml', async () => {
+        const diff = await sourceMapDiffAsHtml({ currentSrc: 'current.js', previousSrc: 'previous.js' });
+        expect(diff).toEqual([
+            {
+                added: false,
+                changeInSize: 9900,
+                currentSize: 10000,
+                isDecreased: false,
+                isIncreased: true,
+                isSame: false,
+                name: 'src/bigger',
+                path: ['src', 'bigger'],
+                previousSize: 100,
+                removed: false
+            },
+            {
+                added: false,
+                changeInSize: -1800,
+                currentSize: 200,
+                isDecreased: true,
+                isIncreased: false,
+                isSame: false,
+                name: 'src/smaller',
+                path: ['src', 'smaller'],
+                previousSize: 2000,
+                removed: false
+            }
+        ]);
+    });
+
+    test('sourceMapDiffForConsole', async () => {
+        const diff = await sourceMapDiffForConsole({ currentSrc: 'current.js', previousSrc: 'previous.js' });
         expect(diff).toEqual([
             {
                 added: false,
